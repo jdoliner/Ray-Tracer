d1e985478700f6f412eb7b1e05e910a3813e8313
Joseph Henry Doliner <jdoliner@antares.cs.uchicago.edu>
2010-02-25 15:57:03 -0600

    Implements Render_Scene which sets up and shoots the rays needed to render a scene.

--- src/scene.c
+++ src/scene.c
@@ -12,9 +12,9 @@
 #include "objects/light.h"
 #include "objects/intersection.h"
 #include "engine/vector.h"
-#include "engine/EasyBMP/EasyBMP.h"
 #include "scene.h"
 #include <float.h>
+#include <assert.h>
 
 /* !Intersect_Scene
  * \brief intersect a ray with an entire scene.
@@ -43,27 +43,30 @@ Intersection_t *Intersect_Scene(Rayf_t *ray, Scene_t *scene) {
 /* !Trace_Ray
  * \brief shoots a ray into a scene and returns the color of the pixel
  */
-Color_t *Trace_Ray(Rayf_t *ray, Scene_t *scene) {
+void Trace_Ray(Rayf_t *ray, Scene_t *scene, Color_t *color) {
     Intersection_t *intersection = Intersect_Scene(ray, scene);
-    Color_t *color = NEW(Vec3f_t);
     CopyColor(intersection->material->diffuse_color, *color);
-    return color;
 }
 
 /* !Render_Scene
  * \brief Shoots rays in to a scene to evaluate their color and returns them as an hres by vres array
+ * \param scene the scene to be rendered
+ * \param wres width resolution (should be a power of 2)
+ * \param hres height resolution (should be a power of 2)
  */
 Color_t *Render_Scene(Scene_t *scene, int wres, int hres) {
     int i, j;
     Color_t *render = NEWVEC(Color_t, wres * hres);
     
     /* first we need to figure out how far away the points the rays intersect the screen at are */
-    float woffsetLength = scene->camera->width / (wres - 1), hoffsetLength = scene->camera->height / (hres - 1);
+    float woffsetLength = scene->camera->width / (wres - 1); /* pixel offset length along width */
+    float hoffsetLength = scene->camera->height / (hres - 1); /* pixel offset length along height */
 
-    Vec3f_t cam_dir;
-    Vec3f_t woffset, hoffset;
+    Vec3f_t cam_dir; /* the direction the camera is pointing */
+    Vec3f_t woffset, hoffset; /* vectors along offset direction */
 
     SubV3f(scene->camera->look_at, scene->camera->pos, cam_dir);
+    NormalizeV3f(cam_dir);
 
     /* notice that this doesn't get messed up of up is not perpendicular to cam_dir */
     CrossV3f(cam_dir, scene->camera->up, woffset);
@@ -76,12 +79,26 @@ Color_t *Render_Scene(Scene_t *scene, int wres, int hres) {
     ScaleV3f(hoffsetLength, hoffset, hoffset);
 
     Rayf_t ray; /* the ray we'll shoot into the scene */
-    CopyV3f(scene->camera->pos, ray->orig);
+    CopyV3f(scene->camera->pos, ray.orig);
+    Vec3f_t screenPos; /* the position on the screen that the ray passes through */
+
+    /* Calculate an initial position in the screen */
+    Vec3f_t centerScreenPos; /* The position of the center of our screen */
+    ScaledAddV3f(scene->camera->pos, scene->camera->focal_length, cam_dir, centerScreenPos);
+
 
     for (i = 0; i < wres; i++) {
 	for (j = 0;j < hres; j++) {
-	    //XXX pick up here XXX
-	    Vec3f_t wscreenpos, hscreenpos;
+	    assert(wres % 2 == 0 && hres % 2 == 0);
+
+	    /* setup the ray */
+	    ScaledAddV3f(centerScreenPos, i - (wres / 2), woffset, screenPos);
+	    ScaledAddV3f(screenPos, j - (hres / 2), hoffset, screenPos);
+	    SubV3f(screenPos, ray.orig, ray.dir);
+	    NormalizeV3f(ray.dir);
+
+	    Trace_Ray(&ray, scene, &render[i + (wres * j)]);
 	}
     }
+    return render;
 }
